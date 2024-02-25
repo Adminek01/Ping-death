977ccf3549f5431af96b3a94b7cc6f1c39bbf22d
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2024-02-25 17:55:42 +0100

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
 
+# Funkcja do wyświetlania menu i pobierania opcji
+show_menu() {
+    echo "Wybierz opcję:"
+    echo "1. Test Apache Bench"
+    echo "2. Test ping"
+    echo "3. Monitorowanie wydajności sieciowej"
+    echo "4. Sprawdzanie dostępności portów"
+    echo "5. Analiza logów"
+    echo "6. Testowanie prędkości łącza"
+    echo "0. Wyjście"
+    read option
+}
+
 # Funkcja do uruchamiania testu Apache Bench
 run_ab_test() {
-    ab -n 1000000 -c 1000 -g ab_results.csv https://zszkurzetnik.mobidziennik.pl/ &
+    # Dodanie argumentu do wyboru celu testu
+    echo "Wpisz adres URL strony do testowania:"
+    read url
+    ab -n 1000000 -c 1000 -g ab_results.csv $url &
 }
 
 # Funkcja do uruchamiania testu ping
 run_ping_test() {
-    ping -f -c 65507 -s 65507 -i 0.001 example.pl &
+    # Dodanie argumentu do wyboru celu testu
+    echo "Wpisz adres IP lub nazwę hosta do pingowania:"
+    read host
+    ping -f -c 65507 -s 65507 -i 0.001 $host &
 }
 
 # Funkcja do monitorowania wydajności sieciowej
 network_performance_monitor() {
     echo "Monitorowanie wydajności sieciowej:"
     echo "----------------------------------"
-    iperf -c zszkurzetnik.mobidziennik.pl
+    # Dodanie argumentu do wyboru serwera
+    echo "Wpisz adres IP lub nazwę hosta serwera:"
+    read server
+    iperf -c $server
 }
 
 # Funkcja do sprawdzania dostępności portów
 check_port_availability() {
-    echo "Sprawdzanie dostępności portów:"
-    echo "-------------------------------"
-    nc -zv example.pl 80
-    nc -zv example.pl 443
+    # Dodanie argumentu do wyboru celu testu
+    echo "Wpisz adres IP lub nazwę hosta do sprawdzania:"
+    read host
+    echo "Sprawdzanie portu 80:"
+    nc -zv $host 80
+    echo "Sprawdzanie portu 443:"
+    nc -zv $host 443
 }
 
 # Funkcja do analizy logów
 analyze_logs() {
     echo "Analiza logów:"
     echo "--------------"
-    # Dodaj tutaj kod do analizy logów, generowania raportów itp.
+    # Dodanie kodu do analizy logów, generowania raportów itp.
 }
 
 # Funkcja do testowania prędkości łącza
@@ -39,23 +64,35 @@ speed_test() {
     speedtest-cli --server 1234
 }
 
-# Uruchomienie testu Apache Bench
-run_ab_test
-
-# Uruchomienie testu ping
-run_ping_test
-
-# Monitorowanie wydajności sieciowej
-network_performance_monitor
-
-# Sprawdzanie dostępności portów
-check_port_availability
-
-# Analiza logów
-analyze_logs
-
-# Testowanie prędkości łącza
-speed_test
-
-# Oczekiwanie na zakończenie wszystkich testów
-wait
+# Główna pętla programu
+while true; do
+    show_menu
+    case "$option" in
+    1)
+        run_ab_test
+        ;;
+    2)
+        run_ping_test
+        ;;
+    3)
+        network_performance_monitor
+        ;;
+    4)
+        check_port_availability
+        ;;
+    5)
+        analyze_logs
+        ;;
+    6)
+        speed_test
+        ;;
+    0)
+        echo "Wyjście z programu..."
+        break
+        ;;
+    *)
+        echo "Nieprawidłowa opcja. Spróbuj ponownie."
+        ;;
+    esac
+    wait
+done
